3a241ba6e6058237ed2a83d0b864e2bd8c5b5428
isavega <igvega02@gmail.com>
2024-05-02 16:59:27 -0400

    feat: add fonts

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text } from "react-native";
+import { useFonts } from "expo-font";
+import HomeScreen from "./screens/HomeScreen/HomeScreen.tsx";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Crypto Neat</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    PoppinsBold: require("./assets/fonts/PoppinsBold.ttf"),
+    PoppinsRegular: require("./assets/fonts/PoppinsRegular.ttf"),
+  });
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+  return <HomeScreen />;
+}
