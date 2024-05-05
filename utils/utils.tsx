920a364b4ddc80a11e9018ea5bcd1b9de58c7401
isavega <igvega02@gmail.com>
2024-05-05 17:27:46 -0400

    feat: generate random success rate function

--- utils/utils.tsx
+++ utils/utils.tsx
@@ -2,3 +2,14 @@
 export const generateRandomNumber = () => {
     return Math.floor(Math.random() * 100000) + 100
 }
+
+export const formatToUSD = (amount: number): string => {
+    const parts = amount?.toFixed(2).toString().split('.')
+    const integerPart = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    return `${integerPart}.${parts[1]}`
+}
+
+export const generateRandomSuccessRate = (): boolean => {
+    const randomValue = Math.random()
+    return randomValue <= 0.9
+}
