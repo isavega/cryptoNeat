073e3f143055a43dd3f50fa7992330c2f281712c
isavega <igvega02@gmail.com>
2024-05-05 17:28:11 -0400

    feat: add database config

--- services/auth.js
+++ services/auth.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
     apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
@@ -9,7 +10,9 @@ const firebaseConfig = {
     messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
     measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
 }
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
+export const database = getDatabase(app)
